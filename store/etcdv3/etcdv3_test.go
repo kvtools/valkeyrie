4bff5525782ab4beba506680fae73bcab0a8c09e
Alexandre Beslic <abeslic@abronan.com>
2017-08-21 00:45:39 +0200

    add initial etcd v3 api support
    Signed-off-by: Alexandre Beslic <abeslic@abronan.com>

---
+++ store/etcdv3/etcdv3_test.go
@@ -0,0 +1,58 @@
+package etcdv3
+
+import (
+	"testing"
+	"time"
+
+	"github.com/docker/libkv"
+	"github.com/docker/libkv/store"
+	"github.com/docker/libkv/testutils"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	client = "localhost:4001"
+)
+
+func makeEtcdV3Client(t *testing.T) store.Store {
+	kv, err := New(
+		[]string{client},
+		&store.Config{
+			ConnectionTimeout: 3 * time.Second,
+			Username:          "test",
+			Password:          "very-secure",
+		},
+	)
+
+	if err != nil {
+		t.Fatalf("cannot create store: %v", err)
+	}
+
+	return kv
+}
+
+func TestRegister(t *testing.T) {
+	Register()
+
+	kv, err := libkv.NewStore(store.ETCDV3, []string{client}, nil)
+	assert.NoError(t, err)
+	assert.NotNil(t, kv)
+
+	if _, ok := kv.(*EtcdV3); !ok {
+		t.Fatal("Error registering and initializing etcd with v3 client")
+	}
+}
+
+func TestEtcdV3Store(t *testing.T) {
+	kv := makeEtcdV3Client(t)
+	lockKV := makeEtcdV3Client(t)
+	ttlKV := makeEtcdV3Client(t)
+
+	testutils.RunTestCommon(t, kv)
+	testutils.RunTestAtomic(t, kv)
+	testutils.RunTestWatch(t, kv)
+	testutils.RunTestLock(t, kv)
+	testutils.RunTestLockTTL(t, kv, lockKV)
+	testutils.RunTestTTL(t, kv, ttlKV)
+	testutils.RunCleanup(t, kv)
+}
