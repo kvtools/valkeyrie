9a40c0d8069c6023d0962384353f629f98d9b755
Tom Moulard <tom.moulard@traefik.io>
2022-07-08 17:31:32 +0200

    fix: clean up supportedBackend (#67)

--- valkeyrie.go
+++ valkeyrie.go
@@ -10,32 +10,32 @@ import (
 )
 
 // Initialize creates a new Store object, initializing the client.
-type Initialize func(addrs []string, options *store.Config) (store.Store, error)
+type Initialize func(endpoints []string, options *store.Config) (store.Store, error)
 
-var (
-	// Backend initializers.
-	initializers = make(map[store.Backend]Initialize)
-
-	supportedBackend = func() string {
-		keys := make([]string, 0, len(initializers))
-		for k := range initializers {
-			keys = append(keys, string(k))
-		}
-		sort.Strings(keys)
-		return strings.Join(keys, ", ")
-	}()
-)
+// Backend initializers.
+var initializers = make(map[store.Backend]Initialize)
 
 // NewStore creates an instance of store.
-func NewStore(backend store.Backend, addrs []string, options *store.Config) (store.Store, error) {
+func NewStore(backend store.Backend, endpoints []string, options *store.Config) (store.Store, error) {
 	if init, exists := initializers[backend]; exists {
-		return init(addrs, options)
+		return init(endpoints, options)
 	}
 
-	return nil, fmt.Errorf("%w %s", store.ErrBackendNotSupported, supportedBackend)
+	return nil, fmt.Errorf("%w %s", store.ErrBackendNotSupported, supportedBackend())
 }
 
 // AddStore adds a new store backend to valkeyrie.
 func AddStore(backend store.Backend, init Initialize) {
 	initializers[backend] = init
+}
+
+// supportedBackend returns a comma separated list of all available stores in initializers.
+func supportedBackend() string {
+	keys := make([]string, 0, len(initializers))
+	for k := range initializers {
+		keys = append(keys, string(k))
+	}
+
+	sort.Strings(keys)
+	return strings.Join(keys, ", ")
 }
