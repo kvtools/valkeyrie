635ee176b19f5ec936274b11fb28b407b4e85274
Tom Moulard <tom.moulard@traefik.io>
2022-07-08 16:58:56 +0200

    fix: allow supportedBackend to return something more than just ""

--- valkeyrie_test.go
+++ valkeyrie_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/kvtools/valkeyrie/store"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewStoreUnsupported(t *testing.T) {
@@ -21,4 +22,48 @@ func TestNewStoreUnsupported(t *testing.T) {
 	assert.Error(t, err)
 	assert.Nil(t, kv)
 	assert.Equal(t, "Backend storage not supported yet, please choose one of ", err.Error())
+}
+
+func TestListSupportedBackends(t *testing.T) {
+	testCases := []struct {
+		desc   string
+		stores []string
+		expect string
+	}{
+		{
+			desc: "no store",
+		},
+		{
+			desc:   "one store",
+			stores: []string{"foo"},
+			expect: "foo",
+		},
+		{
+			desc:   "two unsorted stores",
+			stores: []string{"foo", "bar"},
+			expect: "bar, foo",
+		},
+		{
+			desc:   "two sorted stores",
+			stores: []string{"bar", "foo"},
+			expect: "bar, foo",
+		},
+	}
+
+	for _, test := range testCases {
+		test := test
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+			t.Cleanup(func() { initializers = make(map[store.Backend]Initialize) })
+
+			for _, s := range test.stores {
+				AddStore(store.Backend(s), func(_ []string, _ *store.Config) (store.Store, error) { return nil, nil })
+
+				kv, err := NewStore(store.Backend(s), nil, nil)
+				require.NoError(t, err)
+				require.Nil(t, kv) // AddStore return nil
+			}
+			assert.Equal(t, test.expect, supportedBackend())
+		})
+	}
 }
